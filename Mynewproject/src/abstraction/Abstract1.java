2454cc84dca05d838cad7911283a2772f00895f9
DELL <DELL@DESKTOP-GQ9NT2P>
2022-08-20 23:11:49 +0530

    work comitted

---
+++ Mynewproject/src/abstraction/Abstract1.java
@@ -0,0 +1,33 @@
+package abstraction;
+
+
+ abstract class Simple1
+{
+	 void show()
+	 {
+		 
+	 }
+	 
+	 abstract void show_features();
+	
+}
+ 
+ class Complex extends Simple1
+ {
+
+	@Override
+	void show_features() {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+ }
+
+public class Abstract1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
